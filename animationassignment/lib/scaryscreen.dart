f96f68bd3023863925ad5d474c1d3ec2e17a7fa1
tidalcurrent <sidneysteward@gmail.com>
2024-10-08 18:24:55 -0400

    timer folder

--- animationassignment/lib/scaryscreen.dart
+++ animationassignment/lib/scaryscreen.dart
@@ -19,131 +19,20 @@ class _ScaryscreenState extends State<Scaryscreen> {
   int red = 255;
   int green = 255;
   int blue = 255;
-  String backgroundPlaceholder = "parkbg.png";
-  String petImagePlaceholder = "petimage.png";
-  String _now = "";
-  Timer? _everySecond;
-  int _countdown = 10;
 
-//BEGINNING OF MOOD RELATED FUNCTIONS
-// Function to increase happiness and update hunger when playing with the pet
-  void _playWithPet() {
-    setState(() {
-      if (_countdown != 0) {
-        happinessLevel = (happinessLevel + 10).clamp(0, 100);
-        _updateHunger();
-      }
-      _updateMood();
-      _updateColor();
-    });
-  }
+  //images
+  String backgroundPlaceholder = "bookshelfbg.png";
+  String item1 = "books.png";
+  String item2 = "books2.png";
+  String item3 = "books3.png";
+  String item4 = "candles.png";
+  String item5 = "hangingbat.png";
+  String item6 = "jackolantern.png";
+  String item7 = "pumpkin.png";
+  String item8 = "rat.png";
+  String item9 = "skull.png";
 
-// Function to decrease hunger and update happiness when feeding the pet
-  void _feedPet() {
-    setState(() {
-      if (_countdown != 0) {
-        hungerLevel = (hungerLevel - 10).clamp(0, 100);
-        _updateHappiness();
-      }
-      _updateMood();
-      _updateColor();
-    });
-  }
 
-// Update happiness based on hunger level
-  void _updateHappiness() {
-    if (hungerLevel < 30) {
-      happinessLevel = (happinessLevel - 20).clamp(0, 100);
-    } else {
-      happinessLevel = (happinessLevel + 10).clamp(0, 100);
-    }
-    _updateMood();
-    _updateColor();
-  }
-
-// Increase hunger level slightly when playing with the pet
-  void _updateHunger() {
-    hungerLevel = (hungerLevel + 5).clamp(0, 100);
-    if (hungerLevel > 100) {
-      hungerLevel = 100;
-      happinessLevel = (happinessLevel - 20).clamp(0, 100);
-    }
-    _updateColor();
-  }
-
-  void _updateMood() {
-    if (happinessLevel > 70) {
-      mood = "Happy";
-      petImagePlaceholder = "coolcat.png";
-      backgroundPlaceholder = "dolphinbg.png";
-    } else if (happinessLevel >= 30 && happinessLevel <= 70) {
-      mood = "Neutral";
-      petImagePlaceholder = "petimage.png";
-      backgroundPlaceholder = "parkbg.png";
-    } else {
-      mood = "Unhappy";
-      petImagePlaceholder = "angrypetimage.png";
-      backgroundPlaceholder = "explosionbg.png";
-    }
-  }
-
-  void _updateColor() {
-    if (happinessLevel < 30) {
-      //red
-      green = 50;
-      blue = 50;
-      red = 255;
-    } else if (happinessLevel > 70) {
-      //green
-      red = 50;
-      green = 255;
-      blue = 50;
-    } else {
-      //yellow
-      red = 255;
-      green = 255;
-      blue = 50;
-    }
-  }
-  //END OF MOOD RELATED FUNCTIONS
-
-//BEGINNING OF TIMER RELATED FUNCTIONS
-  void _countDownTimer(int seconds) {
-    _countdown = 10;
-
-    _everySecond = Timer.periodic(Duration(seconds: seconds), (Timer t) {
-      setState(() {
-        if (_countdown > 0) {
-          _countdown--;
-          if (hungerLevel < 100) {
-            hungerLevel += 15;
-          }
-          _now = '$petName is $mood\n Current Timer: $_countdown';
-        } else {
-          t.cancel();
-          if (happinessLevel > 70) {
-            _now = "Winner";
-          } else if (happinessLevel >= 30 && happinessLevel <= 70) {
-            _now = "Meh";
-          } else {
-            _now = "GAME OVER";
-          }
-        }
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _now = '$petName is $mood\n Current Timer: $_countdown';
-    _countDownTimer(1);
-  }
-
-  void _restart() {
-    _now = '$petName is $mood\n Current Timer: $_countdown';
-    _countDownTimer(1);
-  }
   //END OF TIMER RELATED FUNCTIONS
 
   //BEGINNING OF STYLES
@@ -154,57 +43,6 @@ class _ScaryscreenState extends State<Scaryscreen> {
         color: color);
   }
 
-  TextStyle _getTextStyle() {
-    if (_now.contains("Winner") && _countdown == 0) {
-      return GoogleFonts.pixelifySans(
-        color: const Color.fromARGB(255, 15, 136, 15),
-        shadows: [
-          _setShadow(-1.5, -1.5, Colors.white),
-          _setShadow(1.5, -1.5, Colors.white),
-          _setShadow(1.5, 1.5, Colors.white),
-          _setShadow(-1.5, 1.5, Colors.white),
-        ],
-        fontSize: 60,
-        fontWeight: FontWeight.bold,
-      );
-    } else if (_now.contains("Meh") && _countdown == 0) {
-      return GoogleFonts.pixelifySans(
-        color: const Color.fromARGB(255, 176, 156, 46),
-        shadows: [
-          _setShadow(-1.5, -1.5, Colors.white),
-          _setShadow(1.5, -1.5, Colors.white),
-          _setShadow(1.5, 1.5, Colors.white),
-          _setShadow(-1.5, 1.5, Colors.white),
-        ],
-        fontSize: 60,
-        fontWeight: FontWeight.bold,
-      );
-    } else if (_now.contains("GAME OVER") && _countdown == 0) {
-      return GoogleFonts.pixelifySans(
-        color: const Color.fromARGB(255, 161, 8, 8),
-        shadows: [
-          _setShadow(-1.5, -1.5, Colors.black),
-          _setShadow(1.5, -1.5, Colors.black),
-          _setShadow(1.5, 1.5, Colors.black),
-          _setShadow(-1.5, 1.5, Colors.black),
-        ],
-        fontSize: 60,
-        fontWeight: FontWeight.bold,
-      );
-    } else {
-      return GoogleFonts.pixelifySans(
-        color: const Color.fromARGB(255, 253, 253, 253),
-        shadows: [
-          _setShadow(-1.5, -1.5, Colors.black),
-          _setShadow(1.5, -1.5, Colors.black),
-          _setShadow(1.5, 1.5, Colors.black),
-          _setShadow(-1.5, 1.5, Colors.black),
-        ],
-        fontSize: 30.0,
-        fontWeight: FontWeight.w500,
-      );
-    }
-  }
 
   Widget imageButtons(String imagePath, void Function()? buttonFunction) {
     return Container(
@@ -255,13 +93,9 @@ class _ScaryscreenState extends State<Scaryscreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              _now,
-              style: _getTextStyle(),
-            ),
 
             Image.asset(
-              "assets/images/" + petImagePlaceholder,
+              "assets/images/" + item1,
               width: 300,
               color: Color.fromRGBO(red, green, blue, 1),
               colorBlendMode: BlendMode.modulate,
