084cc9114660397f375c95110533457edeb45850
tidalcurrent <sidneysteward@gmail.com>
2024-10-08 18:41:54 -0400

    main changes

--- animationassignment/lib/main.dart
+++ animationassignment/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'scaryscreen.dart';
+import 'imageButtons.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    home: Scaryscreen(),
+  ));
+
 }
