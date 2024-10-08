2f1b04679d4b991b7ed0bd28353a7053eb44179f
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-10-08 19:40:31 -0400

    added buttons into grid

--- animationassignment/lib/scaryscreen.dart
+++ animationassignment/lib/scaryscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animationassignment/imageButtons.dart';
+import 'package:just_audio/just_audio.dart';
 import 'dart:async';
 
 class Scaryscreen extends StatefulWidget {
@@ -13,14 +14,6 @@ class Scaryscreen extends StatefulWidget {
 }
 
 class _ScaryscreenState extends State<Scaryscreen> {
-  String petName = "Pickels";
-  int happinessLevel = 50;
-  int hungerLevel = 50;
-  String mood = "Neutral";
-  int red = 255;
-  int green = 255;
-  int blue = 255;
-
   //images
   String backgroundPlaceholder = "bookshelfbg.png";
   String item1 = "books.png";
@@ -33,48 +26,47 @@ class _ScaryscreenState extends State<Scaryscreen> {
   String item8 = "rat.png";
   String item9 = "skull.png";
 
-  //END OF TIMER RELATED FUNCTIONS
-  buttonFunction() {}
-  //BEGINNING OF STYLES
-  Shadow _setShadow(double pointOne, double pointTwo, Color color) {
-    return Shadow(
-        // bottomLeft
-        offset: Offset(pointOne, pointTwo),
-        color: color);
-  }
-
-  Widget imageButtons(String imagePath, void Function()? buttonFunction) {
-    return Container(
-        width: 100,
-        height: 100,
-        padding: const EdgeInsets.all(0),
-        child: ElevatedButton(
-          onPressed: buttonFunction,
-          style: ElevatedButton.styleFrom(
-            shape: const ContinuousRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30))),
-          ),
-          child: const Text(""),
-        ));
-  }
-
   //END OF STYLES
+
+  Widget buttonFunction(String imagePath) {
+    String chosenImage = "";
+    String correctImage = "assets/images/" + item1;
+    return InkWell(
+      onTap: () {
+        chosenImage = imagePath;
+
+        if (chosenImage == correctImage) {
+          print("Button Works");
+        } else {
+          //play audio
+        }
+      },
+      child: Stack(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+              ),
+              borderRadius: BorderRadius.circular(47.0),
+            ),
+            width: 402, //change this
+            height: 250, //change this
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text(
-          'Digital Pet',
+          'Spooky Hunt',
           style: GoogleFonts.pixelifySans(
             color: const Color.fromARGB(255, 222, 214, 214),
-            shadows: [
-              _setShadow(-1.5, -1.5, Colors.black),
-              _setShadow(1.5, -1.5, Colors.black),
-              _setShadow(1.5, 1.5, Colors.black),
-              _setShadow(-1.5, 1.5, Colors.black),
-            ],
             fontSize: 40.0,
             fontWeight: FontWeight.w500,
           ),
@@ -82,44 +74,29 @@ class _ScaryscreenState extends State<Scaryscreen> {
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/" + backgroundPlaceholder),
-            fit: BoxFit.cover,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/" + backgroundPlaceholder),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // imageButtons("assets/images/bookshelfbg.png",   buttonFunction,),
-
-            Image.asset(
-              "assets/images/bookshelfbg.png",
-              width: 300,
-              color: Color.fromRGBO(red, green, blue, 1),
-              colorBlendMode: BlendMode.modulate,
-              key: UniqueKey(), // Forces the widget to reload the image
-            ),
-
-            // Text(
-            //   'Name: $petName',
-            //   style: TextStyle(fontSize: 20.0),
-            // ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Happiness Level: $happinessLevel',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Hunger Level: $hungerLevel',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 32.0),
-          ],
-        ),
-      ),
+          child: GridView(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 60.0,
+              ),
+              children: [
+                buttonFunction("assets/images/$item1"),
+                buttonFunction("assets/images/$item2"),
+                buttonFunction("assets/images/$item3"),
+                buttonFunction("assets/images/$item4"),
+                buttonFunction("assets/images/$item5"),
+                buttonFunction("assets/images/$item6"),
+                buttonFunction("assets/images/$item8"),
+                buttonFunction("assets/images/$item9"),
+              ])),
     );
   }
 }
